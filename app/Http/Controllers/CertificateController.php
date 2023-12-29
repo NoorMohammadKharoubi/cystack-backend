07d7264b77e37933b2a954e6176e960d134a94d7
noor <noor.kharoubi.work@gmail.com>
2023-12-29 04:05:28 -0800

    first work

---
+++ app/Http/Controllers/CertificateController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Response;
+
+class CertificateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $json_data = "";
+        // if ($request->q){
+        //     $json_data = file_get_contents('https://crt.sh/?q=' . $request->q . '&output=json');
+        // }
+        if ($request->q){
+            $json_data = file_get_contents('data.json');
+        }
+
+        if ($json_data){
+            $json_data = $obj = json_decode($json_data);
+        }
+        
+        if ((boolean)json_decode(strtolower($request->expired_only))){
+            $filtered = [];
+            $current_time = time();
+            foreach($json_data as $item){
+                if (strtotime($item->not_after) < $current_time){
+                    array_push($filtered, $item);
+                }
+            }
+            $json_data = $filtered;
+        }
+
+        return Response::json($json_data);
+    }
+
+}
